142c1aaabbcf8ff1ddd0de8679162e39d59aa9c5
yezy <yezileliilomo@hotmail.com>
2020-04-05 05:10:41 +0300

    :bookmark:Release v0.1.1

--- drf_guard/__init__.py
+++ drf_guard/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'DRF Guard'
 __description__ = 'Flexible permissions for Django REST Framework'
 __url__ = 'https://github.com/yezyilomo/drf-guard'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Yezy Ilomo'
 __author_email__ = 'yezileliilomo@hotmail.com'
 __license__ = 'MIT'
