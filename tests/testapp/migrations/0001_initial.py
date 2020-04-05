a98c560280cc79aee7a0e76eda8538c57c11b060
yezy <yezileliilomo@hotmail.com>
2020-04-05 04:09:19 +0300

    ::green_heart:Fix CI build

--- tests/testapp/migrations/0001_initial.py
+++ tests/testapp/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0011_update_proxy_permissions'),
+
     ]
 
     operations = [
--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import Group
 from django.contrib.auth.models import AbstractUser
 
 
