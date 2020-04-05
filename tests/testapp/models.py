4620d21b95d109d7439ed64d91c315c99897c6d3
yezy <yezileliilomo@hotmail.com>
2020-04-05 04:21:32 +0300

    ::green_heart:Fix CI build

--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.models import AbstractUser
 
 
