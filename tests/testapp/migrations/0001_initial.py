cedc9ac292c9e8a35428ca30438720989e1bf49c
yezy <yezileliilomo@hotmail.com>
2020-04-05 04:34:06 +0300

    :green_heart:Fix CI build

--- tests/testapp/migrations/0001_initial.py
+++ tests/testapp/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0011_update_proxy_permissions'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
